8ba376c46f12dd630eae98b7e36d0719478d3fad
benyaminsh <benitekser@gmail.com>
2023-10-08 23:26:17 +0330

    add command to vpn bot and fix bug

--- bot/main.py
+++ bot/main.py
@@ -7,8 +7,11 @@ token = config('TOKEN',default='9916efd144ea2cf77e9aa22ee08c50d45e4a62e2')
 base_url = config('BASE_URL', default="http://127.0.0.1:8000")
 api_id = config("API_ID", default=29966682, cast=int)
 api_hash = config("API_HASH", default="c7cee45eb9a296b05c005c25febc6264")
+servers_count = 0
+
 
 app = Client("my_account", api_id=api_id, api_hash=api_hash)
+
 
 
 def send_to_server(config, channel):
@@ -39,9 +42,9 @@ def get_channels():
         try:
             username = str(res['username'])
             result.append(username)
-            app.start()
-            app.join_chat(username[1:])
-            app.stop()
+            # app.start()
+            # app.join_chat(username[1:])
+            # app.stop()
         except:
             pass
 
@@ -61,17 +64,41 @@ def find_config(text):
     return False
 
 
-channels_username = get_channels()
+def get_status():
+    message = f"""
+Server Count : {servers_count}
+Channels Count : {len(get_channels())}
+Channels ID : 
+""" + "\n".join(get_channels())
+
+    return message
 
 
-@app.on_message(filters.chat(chats=channels_username))
-async def hello(client, message):
+
+@app.on_message(filters.private)
+async def find_command(client, message):
+    if message.text in commands.keys():
+        await message.reply(commands[message.text])
+
+channels = [].extend(get_channels())
+
+@app.on_message(filters.chat(chats=channels))
+async def main(client, message):
     try:
         configs = find_config(message.text)
         for config in configs:
             send_to_server(config, message.sender_chat.username)
+            global servers_count
+            servers_count += 1
     except:
         pass
 
+
+
+
+commands = {
+    "ping": "Pong -!",
+    "status": get_status(),
+}
 
 app.run()
